c2a666194cfb7f56bacdb221569f513785d52f49
Amin Dorostanian <amin@correctan.com>
2015-03-09 20:07:03 +0200

    https://github.com/dexafree/MaterialList libary added in order to use  for Post Cards.

--- app/src/main/java/com/ozu/ozmo/ozmopol/FragmentFrontPage.java
+++ app/src/main/java/com/ozu/ozmo/ozmopol/FragmentFrontPage.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.view.CardViewNative;
+import com.dexafree.materialList.cards.SmallImageCard;
+import com.dexafree.materialList.view.MaterialListView;
 
 
 /**
@@ -22,14 +22,27 @@ public class FragmentFrontPage extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //Create a Card
-        //Card card =(Card) getView().findViewById(R.id.carddemo);
 
 
-        return inflater.inflate(R.layout.my_fragment, container, false);
+    return inflater.inflate(R.layout.fragment_front_page, container, false);
 
 
 
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MaterialListView mListView = (MaterialListView) getView().findViewById(R.id.material_listview);
+
+        SmallImageCard card = new SmallImageCard(getActivity());
+        card.setDescription("HEY");
+        card.setTitle("Title");
+        card.setDrawable(R.drawable.ic_launcher);
+
+        mListView.add(card);
+
+    }
 }
+

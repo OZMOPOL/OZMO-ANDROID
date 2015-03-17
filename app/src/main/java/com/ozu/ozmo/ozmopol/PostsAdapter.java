ac111a7f67c3a87d37f4c37c75ce845b90439d94
arcadesmasher <deniz.ankay@ozu.edu.tr>
2015-03-17 17:16:40 +0200

    Functionality of objects within PostsAdapter
    The relationship between the PostsAdapter.java & post_item.xml was
    attempted to be initialized. The actions for the buttons & texts in a
    post were attempted to be drafted. A further issue will be opened about
    connecting the methods to where they are meant to be.

--- app/src/main/java/com/ozu/ozmo/ozmopol/PostsAdapter.java
+++ app/src/main/java/com/ozu/ozmo/ozmopol/PostsAdapter.java
@@ -2,10 +2,12 @@ package com.ozu.ozmo.ozmopol;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,19 +15,21 @@ import java.util.List;
 /**
  * Created by amind on 3/9/15.
  */
+
 public class PostsAdapter extends BaseAdapter {
 
     static class PostCardViewHolder {
          TextView tv;
     }
+
     private Activity mContext;
     private List<String> mList;
     private LayoutInflater mLayoutInflater = null;
+
     public PostsAdapter(Activity context, List<String> list) {
         mContext = context;
         mList = list;
-        mLayoutInflater = (LayoutInflater) mContext
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mLayoutInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -61,4 +65,58 @@ public class PostsAdapter extends BaseAdapter {
 
         return convertView;
     }
+
+    //added on 17.03.2015 - 16.59
+    //where should those functions be run at, in order to
+    //give functionality to the elements of
+    //each post at the front page?
+
+    public void setVoteUpButton(){
+        ImageButton voteUp = (ImageButton)getItem(R.id.tv_vote_up);
+        voteUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void setVoteDownButton(){
+        ImageButton voteDown = (ImageButton)getItem(R.id.tv_vote_down);
+        voteDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void setGoToPostButton(){
+        ImageButton go2post = (ImageButton)getItem(R.id.tv_go2post);
+        go2post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void setVoteCount(){
+        int votes = 0; //the info. for the votes is to be obtained from the server.
+        TextView voteCount = (TextView)getItem(R.id.tv_votes);
+        voteCount.setText(votes + "");
+    }
+
+    public void setPostTitle(){
+        String title = "a"; //the info. for the post is to be obtained from the server.
+        TextView postTitle = (TextView)getItem(R.id.tv_post_title);
+        postTitle.setText(title + "");
+    }
+
+    public void setPostText(){
+        String text = "a"; //the info. for the post is to be obtained from the server.
+        TextView postText = (TextView)getItem(R.id.tv_post_content);
+        postText.setText(text + "");
+    }
+
 }

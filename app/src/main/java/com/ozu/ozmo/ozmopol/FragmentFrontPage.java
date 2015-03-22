2dc93dee8352cf56b4ab3bfdf8301dd0b0b3a69d
arcadesmasher <deniz.ankay@ozu.edu.tr>
2015-03-22 17:18:30 +0200

    Portrait/landscape differentiation + tablet/phone differentiation for the "Front Page" is added as well.
    Issue #4 solved completely, case closed.

--- app/src/main/java/com/ozu/ozmo/ozmopol/FragmentFrontPage.java
+++ app/src/main/java/com/ozu/ozmo/ozmopol/FragmentFrontPage.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +26,8 @@ import java.util.List;
  * Created by neokree on 24/11/14.
  */
 public class FragmentFrontPage extends Fragment {
+
+    StaggeredGridView gridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -50,9 +54,31 @@ public class FragmentFrontPage extends Fragment {
 
         PostsAdapter pAdapter=new PostsAdapter(getActivity(),myCards);
 
-        StaggeredGridView gridView = (StaggeredGridView)getView().findViewById(R.id.grid_view);
+        gridView = (StaggeredGridView)getView().findViewById(R.id.grid_view);
         gridView.setAdapter(pAdapter);
+        updateColumnCountForFrontPage();
 
+    }
+
+    public boolean isTablet(){
+        DisplayMetrics metrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        if (width > 720 && height > 1184){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void updateColumnCountForFrontPage(){
+        if(isTablet()){
+            Log.d("Tablet spotted", "sizes won't change");
+        } else {
+            gridView.setColumnCountLandscape(1);
+            gridView.setColumnCountPortrait(1);
+        }
     }
 }
 
